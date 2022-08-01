b9748230fba695ae67ba3e43ff4f2217d6eba79e
KafeinTR <kafein40@gmail.com>
2022-08-01 17:30:22 +0300

    Improvements

--- src/main/java/me/kafein/elitegenerator/EliteGenerator.java
+++ src/main/java/me/kafein/elitegenerator/EliteGenerator.java
@@ -78,9 +78,8 @@ public final class EliteGenerator extends JavaPlugin {
 
         generatorManager.getFeatureManager().getRegenManager().shutdown();
 
-        generatorManager.saveGenerators();
-
         userManager.saveUsers();
+        generatorManager.saveGenerators();
 
     }
 
